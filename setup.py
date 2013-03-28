c711244260d02ba34f52e8f2ab9d264349700b9a
Danny Cosson <dcosson@gmail.com>
2013-03-28 12:26:17 -0400

    updated the setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from distutils.core import setup
 setup(
     name="tornado-stub-client",
+    version="0.1",
+    author="Danny Cosson",
+    author_email="support@venmo.com",
+    license="MIT",
     description="Stubs out tornado AsyncHTTPClient.fetch with a nice interface, for testing code that relies on async code",
-    version='0.1',
+    long_description=open("README.md").read(),
+    url="https://github.com/venmo/tornado-stub-client",
     packages=["tornado_stub_client"],
+    install_requires=[
+        "tornado>=2.0",
+    ],
 )
